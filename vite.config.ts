0f29a3aeee84da6e3e5381ace3f7fa52c57b25b0
TheCodeTherapy <me@mgz.me>
2024-04-25 13:35:23 +0100

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,104 @@
+import { defineConfig } from "vite";
+import { viteSingleFile } from "vite-plugin-singlefile";
+import { createHtmlPlugin } from "vite-plugin-html";
+import { Plugin } from "vite";
+import { optimize } from "svgo";
+import fs from "fs";
+import path from "path";
+import mime from "mime-types";
+
+const execDir = process.cwd();
+
+function viteBase64ImagePlugin(): Plugin {
+  return {
+    name: "vite-base64-image-plugin",
+    enforce: "pre",
+    load(id: string) {
+      if (
+        id.startsWith("vite:") ||
+        !id.startsWith("/") ||
+        id.includes("\x00")
+      ) {
+        return null; // Skip handling non-filesystem or virtual paths
+      }
+
+      const extension = path.extname(id);
+      const mimeType = mime.lookup(extension) || "application/octet-stream";
+
+      if (extension === ".svg") {
+        const svgContent = fs.readFileSync(id, "utf8");
+        let optimized: string | null = null;
+        try {
+          const optimizedContent = optimize(svgContent, {
+            plugins: [
+              {
+                name: "preset-default",
+                params: { overrides: { removeViewBox: false } },
+              },
+              "removeComments",
+              "cleanupIds",
+            ],
+            multipass: true,
+          });
+          optimized = optimizedContent.data;
+        } catch (error) {
+          console.log(
+            `Couldn't optimize ${id} SVG file. Using unoptimized version.`
+          );
+        }
+        const svg = optimized !== null ? optimized : svgContent;
+        const encodedSvgContent = encodeURIComponent(svg)
+          .replace(/'/g, "%27")
+          .replace(/"/g, "%22");
+        return `export default "data:image/svg+xml;charset=utf-8,${encodedSvgContent}"`;
+      } else {
+        if ([".png", ".jpg", ".jpeg", ".gif"].includes(extension)) {
+          const fileBuffer = fs.readFileSync(id);
+          const base64String = `data:${mimeType};base64,${fileBuffer.toString(
+            "base64"
+          )}`;
+          return `export default "${base64String}"`;
+        }
+      }
+    },
+  };
+}
+
+function viteStripCommentsPlugin(): Plugin {
+  return {
+    name: "vite-strip-comments",
+    enforce: "post",
+    renderChunk(code) {
+      const licenseCommentRegex = /\/\*\*[\s\S]*?@license[\s\S]*?\*\/\n/g;
+      const newCode = code.replace(licenseCommentRegex, "");
+      return { code: newCode, map: null };
+    },
+  };
+}
+
+export default defineConfig({
+  plugins: [
+    viteSingleFile(),
+    createHtmlPlugin({ minify: true }),
+    viteBase64ImagePlugin(),
+    viteStripCommentsPlugin(),
+  ],
+  build: {
+    minify: "terser",
+    cssCodeSplit: false,
+    assetsInlineLimit: 100000000,
+    terserOptions: {
+      mangle: {
+        eval: true,
+        keep_fnames: false,
+        module: true,
+        toplevel: true,
+        safari10: false,
+      },
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+  },
+});
